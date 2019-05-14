f4e4700aa6200713affd676b3dba670bb55f0110
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-05-13 18:07:51 -0700

    testing gitignore

---
+++ scenario1.py
@@ -0,0 +1,10 @@
+"""
+Scenario 1: One-time route cost check
+
+You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a single phone number. How quickly can you find the cost of calling this number?
+
+* dictionary
+* {key, val} = {routing #, price} 
+"""
+
+def 
---
+++ scenario2.py
@@ -0,0 +1,5 @@
+"""
+Scenario 2: List of route costs to check
+
+You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a list of 1000 phone numbers. How can you operationalize the route cost lookup problem?
+"""
