1631265693b132faa688352f58b2550bd98b9243
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-05-13 18:15:04 -0700

    reading the file

--- scenario1.py
+++ scenario1.py
@@ -7,4 +7,11 @@ You have a carrier route list with 100,000 (100K) entries (in arbitrary order) a
 * {key, val} = {routing #, price} 
 """
 
-def 
+import sys
+import re
+
+def file_to_dict(file_name):
+    with open('data/' + file_name, "r") as file:
+        price_guide = file.read()
+        price_guide = re.split(',|\n', price_guide)
+    return price_guide
