aeff6b3e084b942965aaafa0fbde25818bfb8d77
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-13 18:25:53 -0700

    started scenario2

--- scenario2.py
+++ scenario2.py
@@ -1,5 +1,9 @@
 """
 Scenario 2: List of route costs to check
 
-You have a carrier route list with 100,000 (100K) entries (in arbitrary order) and a list of 1000 phone numbers. How can you operationalize the route cost lookup problem?
+You have a carrier route list with 100,000 (100K) entries (in arbitrary order) 
+and a list of 1000 phone numbers. How can you operationalize the route cost lookup problem?
 """
+
+# We can use set here. Set1 = route costs, Set2 = input phone number file with 1000 numbers
+# data structure for set could be hashtable
