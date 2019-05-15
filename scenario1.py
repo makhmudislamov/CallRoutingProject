e06d7305815de855d93d6ce0fd2873480e4c2d7f
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-05-14 18:02:37 -0700

    file to dictionary

--- scenario1.py
+++ scenario1.py
@@ -11,7 +11,34 @@ import sys
 import re
 
 def file_to_dict(file_name):
+    dict = {}
     with open('data/' + file_name, "r") as file:
         price_guide = file.read()
         price_guide = re.split(',|\n', price_guide)
-    return price_guide
+        i = 0 #counter
+        while i < len(price_guide) - 1:
+            entry = price_guide[i]
+            if entry in dict:
+                if dict[entry] > price_guide[i+1]:
+                    dict[entry] = price_guide[i+1]
+            else:
+                if entry[0] == '+':
+                    dict[entry] = price_guide[i+1]
+            i+=1
+            # i+=2
+    return dict
+
+# def call_price(phone_num, price_guide):
+#     for key, value in price_guide:
+#         if key == phone_num:
+#             return value
+#         for i in range(len(phone_num)-1):
+#             if key[i] == phone_num[i]:
+#                 return value
+#         return None
+
+if __name__ == "__main__":
+    path = file_to_dict('route-costs-10.txt')
+    print(path)
+    # target_price = call_price('+86153', path)
+    # print(target_price)
